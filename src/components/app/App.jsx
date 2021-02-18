2af78425992ff01727a0f662be6d467aac9abe70
Katelinn Jones <katiejonesyo@gmail.com>
2021-02-17 22:41:09 -0800

    selector and actions started, start app.js

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import PostForm from '../PostForm/PostForm';
+
 
 export default function App() {
-  return <h1>Hello World</h1>;
-}
+  return (
+    <PostForm />
+  );
+};
+
+
+
