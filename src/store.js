19b7fd805032f57c9d9728b4b0549bc539249799
Katelinn Jones <katiejonesyo@gmail.com>
2021-02-18 21:34:03 -0800

    create store.js and imports

---
+++ src/store.js
@@ -0,0 +1,10 @@
+
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import postReducer from './reducers/postReducers';
+
+const store = createStore(postReducer, composeWithDevTools());
+
+export default store;
+
+
